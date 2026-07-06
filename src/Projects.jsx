246759d5de4cf6027284a19c7acf4699c0399a4a
Sarinna <57844555+ssung01-pixel@users.noreply.github.com>
2026-07-06 18:20:50 -0400

    Add a coding project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,6 +5,12 @@ const Projects = () => {
   return (
     <div>
       <Card
+        title="Cybersecurity Log Analyzer powered by Claude"
+        body="This project simulates a Security Operations Center (SOC) workflow by automatically identifying suspicious login behavior from uploaded log files. The application combines rule-based detection with Large Language Model (LLM) analysis to provide security teams with actionable insights."
+        link="https://github.com/sarinnasung/cybersecurity-log-analyzer-powered-by-claude"
+      />
+
+      <Card
         title="Munchies"
         body="This web application that allows users to select their preferred restaurants and receive recommended activities in the surrounding area by utilizing Yelp Restaurant API and Decathlon API. Created wireframes, planned use cases, and conducted user testing to ensure a seamless user experience. Languages used: React, Node.js, MySQL"
         link="https://github.com/sarinnasung/CS411project"
